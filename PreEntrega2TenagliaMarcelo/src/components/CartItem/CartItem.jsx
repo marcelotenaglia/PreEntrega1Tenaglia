4a3241a04777f780c2b6c47af3a8ca1d057c3088
marcelotenaglia <tenaco23@gmail.com>
2024-03-11 18:50:11 -0300

    diseño

--- PreEntrega2TenagliaMarcelo/src/components/CartItem/CartItem.jsx
+++ PreEntrega2TenagliaMarcelo/src/components/CartItem/CartItem.jsx
@@ -1,3 +1,4 @@
+import "./CartItem.css";
 import { useContext } from "react";
 import { CartContext } from "../../context/CartContext";
 import ItemDetail from "../ItemDetail/ItemDetail";
@@ -6,11 +7,11 @@ export const CartItem = ({ item }) => {
   const { id, name, precio, cantidad } = item;
 
   return (
-    <div>
-      <h2>{name}</h2>
-      <p>Cantidad: {cantidad}</p>
-      <p>Precio x unidad:{precio} </p>
-      <p>Subtotal: {cantidad * precio}</p>
+    <div className="cart-item">
+      <h2 className="title">{name}</h2>
+      <p className="cantidad">Cantidad: {cantidad}</p>
+      <p className="price">Precio x unidad:{precio} </p>
+      <p className="subtotal">Subtotal: {cantidad * precio}</p>
     </div>
   );
 };
